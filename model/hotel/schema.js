a4d0c10bdd6878d24e463e1b57cf0895491ddb7d
augustoalegon <mac@MacBook-Pro-de-Mac.local>
2018-04-09 15:19:36 -0500

    Defining the schema for data base

--- model/hotel/schema.js
+++ model/hotel/schema.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const hotelSchema = new Schema({
-  title: { type: String, required: true },
-  body: { type: String }
+  id: { type: String, required: true },
+  name: { type: String },
+  starts: { type: Integer },
+  price: { type: Integer },
+  images: { type: String },
+  amenities: { type: Array}
 })
 
 module.exports = hotelSchema
